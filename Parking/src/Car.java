1b4932aa2b9e1aa48b31618b47f2181fe5c01c2c
Ivan <Ivan@10.0.0.129>
2019-10-18 23:22:09 -0500

    Added waitlist, parked, updated print

--- Parking/src/Car.java
+++ Parking/src/Car.java
@@ -3,6 +3,7 @@ public class Car {
 	String Make;
 	String Model;
 	String timeParking;
+	int isPark = 0;
 	
 	public void CarMake(String s) {
 		this.Make = s;
@@ -13,10 +14,24 @@ public class Car {
 	public void CarTime(String s) {
 		this.timeParking = s;
 	}
+	public void cParked() {
+		this.isPark = 1;
+	}
+	public void cWait() {
+		this.isPark = 2;
+	}
+	
 	public void CarPrint() {
 		double price = Integer.parseInt(timeParking);
 		price = price * 0.05;
-		System.out.println("The " + Make + " " + Model + " has parked for a total of " + timeParking + " minutes.");
-		System.out.println("The " + Make + " " + Model + " check out total is $" + price + ".");
+		if (isPark == 1) {
+			System.out.println("The " + Make + " " + Model + " has parked for a total of " + timeParking + " minutes.");
+			System.out.println("The " + Make + " " + Model + " check out total is $" + price + ".");
+			System.out.println();
+		}
+		if (isPark == 2) {
+			System.out.println("The " + Make + " " + Model + " is waiting for a parking spot to open.");
+			System.out.println();
+		}
 	}
 }
--- Parking/src/Parking.java
+++ Parking/src/Parking.java
@@ -3,13 +3,15 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Parking {
 	
-	static int capacity = 10;
-	static Car[] carArr = new Car[10];
+	static int capacity = 5;
+	static Car[] carArr = new Car[99];
 	
 	public static void main(String[] args) {
 		Scanner scanner = null;
-		int count = 0;
+		int count = 0; //used for iteration
 		int totalCars = 0;
+		int waitline = 0;
+		int parked = 0;
 		try {
 			scanner = new Scanner(new File("C:\\Users\\Ivan\\git\\parking\\Parking\\src\\input.txt"));
 		} catch (FileNotFoundException e) {
@@ -21,6 +23,14 @@ public class Parking {
 				//totalCars++;
 				carArr[totalCars] = new Car();
 				carArr[totalCars].CarMake(line);
+				if (parked >= capacity) {
+					waitline++;
+					carArr[totalCars].cWait();
+				}
+				if (parked < capacity) {
+					parked++;
+					carArr[totalCars].cParked();
+				}
 			}
 			if (count == 1) {
 				carArr[totalCars].CarModel(line);
@@ -35,11 +45,11 @@ public class Parking {
 				count = 0;
 			}
 		}
-		
+
 		for (int i=0; i<totalCars; i++) {
 			carArr[i].CarPrint();
 		}
-
+		System.out.println("There is " + waitline + " vehicle(s) waiting for a spot to open.");
 	}
 
 }
