c7baf6447bede52c0c126d1d878fbadc0feb6767
Ivan <Ivan@10.0.0.129>
2019-10-18 22:12:05 -0500

    input file added

--- Parking/src/Parking.java
+++ Parking/src/Parking.java
@@ -1,8 +1,22 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Parking {
-
+	
+	int capacity = 10;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new File("C:\\Users\\Ivan\\git\\parking\\Parking\\src\\input.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while(scanner.hasNextLine()) {
+			String line = scanner.next();
+			System.out.println(line);
+		}
+		
 
 	}
 
