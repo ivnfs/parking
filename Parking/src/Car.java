3a629b9672834d44e4dc22dd9d285f995833b1a0
Ivan <Ivan@10.0.0.129>
2019-10-20 22:18:58 -0500

    Added test cases, comments, minor fixes

--- Parking/src/Car.java
+++ Parking/src/Car.java
@@ -1,9 +1,9 @@
 
-public class Car {
+public class Car {//stores Car data and functions
 	String Make;
 	String Model;
 	String timeParking;
-	int isPark = 0;//status to see if car is parked or waiting
+	int isPark = 0; //status to see if car is parked or waiting
 	
 	public void CarMake(String s) {//function to attach car make
 		this.Make = s;
@@ -23,7 +23,7 @@ public class Car {
 	
 	public void CarPrint() {//prints out car info and time based on if they are parked / waiting
 		double price = Integer.parseInt(timeParking);//switch from string to double
-		price = price * 0.05;//calculates price of stay, rate is 5 cents a minute
+		price = price * 0.05; //calculates price of stay, rate is 5 cents a minute
 		if (isPark == 1) {//if car is successfully parked
 			System.out.println("The " + Make + " " + Model + " is parking for a total of " + timeParking + " minutes.");
 			System.out.println("The " + Make + " " + Model + " check out total is $" + price + ".");
--- Parking/src/Parking.java
+++ Parking/src/Parking.java
@@ -3,8 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Parking {
 	
-	static int capacity = 5;//number of available parking spots
-	static Car[] carArr = new Car[99];//storage of customer car info and time
+	static int capacity = 5; //number of available parking spots, change if needed
+	static Car[] carArr = new Car[99]; //storage of customer car info and time
 	
 	public static Car[] removeIndex(Car[] c, int x) {//remove index from array
 		if (c == null || x < 0 || x >= c.length) {//help on this function from geeksforgeeks.org
@@ -18,7 +18,7 @@ public class Parking {
 	
 	public static void waitPrint(int wait) {//prints wait line data
 		if (wait == 0) {
-			System.out.println("All vehicles have successfully parked.");
+			System.out.println("All vehicle(s) have successfully parked.");
 		}
 		if (wait > 0) {
 			System.out.println("There is " + wait + " vehicle(s) waiting at the entrance gate for a parking spot to open.");
@@ -28,9 +28,9 @@ public class Parking {
 	public static void main(String[] args) {
 		Scanner scanner = null;
 		int count = 0; //used for iteration with scanner
-		int totalCars = 0;//total cars trying to enter
-		int waitline = 0;//cars waiting to park
-		int parked = 0;//cars that have parked
+		int totalCars = 0; //total cars trying to enter
+		int waitline = 0; //cars waiting to park
+		int parked = 0; //cars that have parked
 		try {
 			scanner = new Scanner(new File("C:\\Users\\Ivan\\git\\parking\\Parking\\src\\input.txt"));
 		} catch (FileNotFoundException e) {
@@ -44,11 +44,11 @@ public class Parking {
 				carArr[totalCars].CarMake(line);
 				if (parked >= capacity) {//adds to wait line if parking full
 					waitline++;
-					carArr[totalCars].cWait();//Lets Car class know status of car (waiting)
+					carArr[totalCars].cWait(); //Lets Car class know status of car (waiting)
 				}
 				if (parked < capacity) {//adds to parked if successfully parked
 					parked++;
-					carArr[totalCars].cParked();//Lets Car class know status of car (parked)
+					carArr[totalCars].cParked(); //Lets Car class know status of car (parked)
 				}
 			}
 			if (count == 1) {//takes in model of car
@@ -60,8 +60,8 @@ public class Parking {
 				for (int i=0; i<totalCars-1; i++) {//loops through all cars on file, if exact same entry, car leaves early
 					if (carArr[totalCars-1].Make == carArr[i].Make & carArr[totalCars-1].Model == carArr[i].Model & carArr[totalCars-1].timeParking == carArr[i].timeParking) {
 						System.out.println(carArr[totalCars-1].Make + " " + carArr[totalCars-1].Model + "has exited the lot early!");
-						removeIndex(carArr, i);
-						removeIndex(carArr, totalCars-2);
+						removeIndex(carArr, i); //removes original car from array
+						removeIndex(carArr, totalCars-2); //removes new entry from array, -2 because array shifts after a removal
 					}
 				}
 			}
