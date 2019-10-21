95c0718b3f5308c5222b9aa5307a698df91cc4d1
Ivan <Ivan@10.0.0.129>
2019-10-20 20:23:54 -0500

    Added comments explaining all functionality

--- Parking/src/Car.java
+++ Parking/src/Car.java
@@ -3,33 +3,33 @@ public class Car {
 	String Make;
 	String Model;
 	String timeParking;
-	int isPark = 0;
+	int isPark = 0;//status to see if car is parked or waiting
 	
-	public void CarMake(String s) {
+	public void CarMake(String s) {//function to attach car make
 		this.Make = s;
 	}
-	public void CarModel(String s) {
+	public void CarModel(String s) {//function to attach car model
 		this.Model = s;
 	}
-	public void CarTime(String s) {
+	public void CarTime(String s) {//function to attach time stayed
 		this.timeParking = s;
 	}
-	public void cParked() {
+	public void cParked() {//keeps the individual status of car, so in this case parked = 1
 		this.isPark = 1;
 	}
-	public void cWait() {
+	public void cWait() {//keeps the individual status of car, waiting = 2
 		this.isPark = 2;
 	}
 	
-	public void CarPrint() {
-		double price = Integer.parseInt(timeParking);
-		price = price * 0.05;
-		if (isPark == 1) {
+	public void CarPrint() {//prints out car info and time based on if they are parked / waiting
+		double price = Integer.parseInt(timeParking);//switch from string to double
+		price = price * 0.05;//calculates price of stay, rate is 5 cents a minute
+		if (isPark == 1) {//if car is successfully parked
 			System.out.println("The " + Make + " " + Model + " has parked for a total of " + timeParking + " minutes.");
 			System.out.println("The " + Make + " " + Model + " check out total is $" + price + ".");
 			System.out.println();
 		}
-		if (isPark == 2) {
+		if (isPark == 2) {//if car is waiting
 			System.out.println("The " + Make + " " + Model + " is waiting for a parking spot to open.");
 			System.out.println();
 		}
--- Parking/src/Parking.java
+++ Parking/src/Parking.java
@@ -3,50 +3,50 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Parking {
 	
-	static int capacity = 5;
-	static Car[] carArr = new Car[99];
+	static int capacity = 5;//number of available parking spots
+	static Car[] carArr = new Car[99];//storage of customer car info and time
 	
 	public static void main(String[] args) {
 		Scanner scanner = null;
-		int count = 0; //used for iteration
-		int totalCars = 0;
-		int waitline = 0;
-		int parked = 0;
+		int count = 0; //used for iteration with scanner
+		int totalCars = 0;//total cars trying to enter
+		int waitline = 0;//cars waiting to park
+		int parked = 0;//cars that have parked
 		try {
 			scanner = new Scanner(new File("C:\\Users\\Ivan\\git\\parking\\Parking\\src\\input.txt"));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		while(scanner.hasNextLine()) {
+		while(scanner.hasNextLine()) {//read from input.txt
 			String line = scanner.next();
-			if (count == 0) {
+			if (count == 0) {//takes in the make of car
 				//totalCars++;
 				carArr[totalCars] = new Car();
 				carArr[totalCars].CarMake(line);
-				if (parked >= capacity) {
+				if (parked >= capacity) {//adds to wait line if parking full
 					waitline++;
-					carArr[totalCars].cWait();
+					carArr[totalCars].cWait();//Lets Car class know status of car (waiting)
 				}
-				if (parked < capacity) {
+				if (parked < capacity) {//adds to parked if successfully parked
 					parked++;
-					carArr[totalCars].cParked();
+					carArr[totalCars].cParked();//Lets Car class know status of car (parked)
 				}
 			}
-			if (count == 1) {
+			if (count == 1) {//takes in model of car
 				carArr[totalCars].CarModel(line);
 			}
-			if (count == 2) {
+			if (count == 2) {//takes in time of stay
 				carArr[totalCars].CarTime(line);
-				totalCars++;
+				totalCars++;//when make, model, and time are entered, a car has been added
 			}
 			//System.out.println(line);
 			count++;
-			if (count == 3) {
+			if (count == 3) {//creates an iteration of 3, so data is taken in 3 parts, looped
 				count = 0;
 			}
 		}
 
-		for (int i=0; i<totalCars; i++) {
+		for (int i=0; i<totalCars; i++) {//Prints each cars' info and status
 			carArr[i].CarPrint();
 		}
 		System.out.println("There is " + waitline + " vehicle(s) waiting for a spot to open.");
