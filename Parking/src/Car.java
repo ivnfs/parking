7788941f555933614306810d52e9206b1101af1b
Ivan <Ivan@10.0.0.129>
2019-10-27 21:31:00 -0500

    Added overflow lot functionality

--- Parking/src/Car.java
+++ Parking/src/Car.java
@@ -3,7 +3,7 @@ public class Car {//stores Car data and functions
 	String Make;
 	String Model;
 	String timeParking;
-	int isPark = 0; //status to see if car is parked or waiting
+	int isPark = 0; //status to see if car is parked, waiting, or parked in overflow
 	
 	public void CarMake(String s) {//function to attach car make
 		this.Make = s;
@@ -20,17 +20,27 @@ public class Car {//stores Car data and functions
 	public void cWait() {//keeps the individual status of car, waiting = 2
 		this.isPark = 2;
 	}
+	public void cParked2() {//keeps the individual status of car, overflow parked = 3
+		this.isPark = 3;
+	}
+	
 	
 	public void CarPrint() {//prints out car info and time based on if they are parked / waiting
 		double price = Integer.parseInt(timeParking);//switch from string to double
-		price = price * 0.05; //calculates price of stay, rate is 5 cents a minute
+		double price1 = price * 0.05; //calculates price of stay, rate is 5 cents a minute for lot 1
+		double price2 = price * 0.15; //calculates price of stay for overflow lot 2 rates
 		if (isPark == 1) {//if car is successfully parked
-			System.out.println("The " + Make + " " + Model + " is parking for a total of " + timeParking + " minutes.");
-			System.out.println("The " + Make + " " + Model + " check out total is $" + price + ".");
+			System.out.println("The " + Make + " " + Model + " is parking at lot 1 for a total of " + timeParking + " minutes.");
+			System.out.println("The " + Make + " " + Model + " check out total is $" + price1 + " at standard rates.");
 			System.out.println();
 		}
 		if (isPark == 2) {//if car is waiting
 			System.out.println("The " + Make + " " + Model + " is still waiting at the entrance gate for a parking spot to open.");
+			System.out.println();
+		}
+		if (isPark == 3) {//if parking at overflow at higher rate
+			System.out.println("The " + Make + " " + Model + " is parking at overflow lot 2 for a total of " + timeParking +" minutes.");
+			System.out.println("The " + Make + " " + Model + " check out total is $" + price2 + " at higher rates.");
 			System.out.println();
 		}
 	}
--- Parking/src/Parking.java
+++ Parking/src/Parking.java
@@ -3,7 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Parking {
 	
-	static int capacity = 5; //number of available parking spots, change if needed
+	static int capacity = 3; //number of available parking spots, change if needed
+	static int overflowCapacity = 3; //number of available over flow parking spots, change if needed
 	static Car[] carArr = new Car[99]; //storage of customer car info and time
 	
 	public static Car[] removeIndex(Car[] c, int x) {//remove index from array
@@ -31,6 +32,7 @@ public class Parking {
 		int totalCars = 0; //total cars trying to enter
 		int waitline = 0; //cars waiting to park
 		int parked = 0; //cars that have parked
+		int parked2 = 0; //cars that have parked in overflow
 		try {
 			scanner = new Scanner(new File("C:\\Users\\Ivan\\git\\parking\\Parking\\src\\input.txt"));//to test, add 2-5 between "input" and ".txt"
 		} catch (FileNotFoundException e) {
@@ -39,12 +41,17 @@ public class Parking {
 		while(scanner.hasNextLine()) {//read from input.txt
 			String line = scanner.next();
 			if (count == 0) {//takes in the make of car
-				//totalCars++;
 				carArr[totalCars] = new Car();
 				carArr[totalCars].CarMake(line);
-				if (parked >= capacity) {//adds to wait line if parking full
-					waitline++;
-					carArr[totalCars].cWait(); //Lets Car class know status of car (waiting)
+				if (parked >= capacity) {//adds vehicles to overflow if lot 1 full
+					if (parked2 >= overflowCapacity) {//adds vehicles to wait list if overflow full
+						waitline++;
+						carArr[totalCars].cWait(); //Lets Car class know status of car (waiting)
+					}
+					if (parked2 < overflowCapacity) {//parks in overflow parking lot if under capacity
+						parked2++;
+						carArr[totalCars].cParked2(); //Lets Car class know status of car (parked in overflow)
+					}
 				}
 				if (parked < capacity) {//adds to parked if successfully parked
 					parked++;
